66c81bac36951d085905da6164c880df438952be
NavaTeja2408 <teja29204@gmail.com>
2023-02-08 21:31:51 +0530

    created sample files

--- sample/first/urls.py
+++ sample/first/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index") ,
+    path("abc", views.third, name="abc" ) ,
+    path("new", views.new, name="new" )
 ]
--- sample/first/views.py
+++ sample/first/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+tasks=["Navateja","Manohar","Jyothi","Vinoothna"]
+
 # Create your views here.
 def index(request):
     return HttpResponse("ABC")
+
+def third(request):
+    return render(request, "first/index.html")
+
+def new(request):
+    return render(request, "first/newtasks.html" ,{
+        "tasks":tasks
+    })
